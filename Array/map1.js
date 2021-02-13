daaac71d2d1c9160664468bcdae732aacf00df9f
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2021-02-13 19:48:23 -0300

    Modificando arquivo map2.js

--- Array/map1.js
+++ Array/map1.js
@@ -6,3 +6,15 @@ let resultado = nums.map(function(e) {
 })
 
 console.log(resultado)
+
+// Algumas possibilidades com 'map'
+const soma10 = e => e + 10
+const triplo = e => e * 3
+const paraDinheiro = e => `R$ ${parseFloat(e).toFixed(2).replace('.', ',')}`
+
+// Fazendo encadeamento de 'map'
+resultado = nums.map(soma10).map(triplo).map(paraDinheiro)
+
+
+
+console.log(resultado)
