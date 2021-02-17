6049763f454e8a99284f183c5d7862a0431b965f
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2021-02-16 23:30:04 -0300

    Adicionando arquivo concat.js

---
+++ Array/concat.js
@@ -0,0 +1,5 @@
+// O método 'concat' unifica elementos/arrays em um único array
+const filhas = ['Maria Fernanda', 'Maria Isabela']
+const filhos = ['Davi Lucas', 'Nick Williams']
+const filhosEFilhas = filhas.concat(filhos, 'Sovacolino')
+console.log(filhosEFilhas)
