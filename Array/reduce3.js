78680508ec8f8734332702a302c9c37e375643c7
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2021-02-16 22:11:07 -0300

    Adicionando arquivo reduce3.js

---
+++ Array/reduce3.js
@@ -0,0 +1,12 @@
+//Simulando o funcionamento do método 'reduce'
+Array.prototype.reduce2 = function(callback) {
+    let acumulador = this[0]
+    for (let i = 1; i < this.length; i++) {
+        acumulador = callback(acumulador, this[i], i, this)
+    }
+    return acumulador
+}
+
+const soma = (total, valor) => total + valor
+const nums = [1, 2, 3, 4, 5]
+console.log(nums.reduce(soma))
