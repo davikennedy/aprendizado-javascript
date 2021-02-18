8a3f593777101e027f5c8fd8c5f9365ff3081722
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2021-02-17 23:09:25 -0300

    Modificando arquivo strings.js

--- Fundamentos/strings.js
+++ Fundamentos/strings.js
@@ -1,15 +1,35 @@
-const escola = "Cod3r"
+// Retorna o tamanho de uma string
+const textSize = 'Texto'.length
+console.log(`Quantidade de letras: ${textSize}`)
 
-console.log(escola.charAt(4))
-console.log(escola.charAt(5))
-console.log(escola.charCodeAt(3)) // Retorna o valor da tabela ASCII referente ao caracter
-console.log(escola.indexOf('o')) // Verifica em que posição está o caracter informado
+// Retorna um array quebrando a string por um delimitador
+const splittedText = 'Texto'.split('')
+console.log('Array com as posições separadas pelo delimitador \'\':', splittedText)
 
-console.log(escola.substring(1)) // Retorna a string a partir do valor informado
-console.log(escola.substring(1,3))
+// Busca por um valor e substitui por outro
+const replacedText = 'Texto'.replace('Texto', 'otxeT')
+console.log('Substituição de valor:', replacedText)
 
-console.log('Escola '.concat(escola).concat("!"))
-console.log('Escola ' + escola + ("!"))
-console.log(escola.replace(3, 'e'))
+// Retorna a "fatia" de um valor
+const lastChar = 'Texto'.slice(-1)
+console.log('Última letra da string:', lastChar)
 
-console.log('Ana,Pedro,Aline'.split(','))
+const allWithoutLastChar = 'Texto'.slice(0, -1)
+console.log('Valor da string excluindo o último caractere:', allWithoutLastChar)
+
+const secondToEnd = 'Texto'.slice(1)
+console.log('Valor da string a partir do 2º caractere:', secondToEnd)
+
+// Retorna N caracteres a partir de uma posição
+const nCaracteres = 'Texto'.substring(0, 2)
+console.log('Os dois primeiros caracteres são:', nCaracteres)
+
+// Verifica em que posição está o caracter informado
+const whereIs = "Texto".indexOf('o')
+console.log('O caractere informado está na posição:', whereIs) 
+
+// Concatena strings
+const texto1 = 'Esse é o texto 1'
+const texto2 = ' e '
+const texto3 = 'Esse é o texto 3'
+console.log(texto1.concat(texto2).concat(texto3) + "!")
