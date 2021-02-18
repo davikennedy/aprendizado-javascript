bc1667b755b62a2f8d1fb80bea2f7377cd102cb3
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2021-02-18 16:57:38 -0300

    Adicionando arquivo exercicio6.js

---
+++ Exercícios Práticos/exercicio6.js
@@ -0,0 +1,26 @@
+/*
+    Elabore duas funções que recebem três parâmetros: capital inicial, taxa de juros e tempo de aplicação. A
+    primeira função retornará o montante da aplicação financeira sob o regime de juros simples e a segunda
+    retornará o valor da aplicação sob o regime de juros compostos.
+*/
+
+function jurosSimples(capitalInicial, taxaJuros, tempoAplicacao) {
+    return capitalInicial + (capitalInicial / 100 * taxaJuros * tempoAplicacao)   
+}
+
+function jurosCompostos(capitalInicial, taxaJuros, tempoAplicacao) {
+    let montante = 0
+    for(let i = 1; i <= tempoAplicacao; i++) {
+        montante = capitalInicial + (capitalInicial / 100 * taxaJuros)
+        capitalInicial = montante
+    }
+    return montante.toFixed(2)
+}
+
+const capitalInicial = 100, taxaJuros = 5, tempoAplicacao = 6
+
+const calculaJurosSimples = jurosSimples(capitalInicial, taxaJuros, tempoAplicacao)
+const calculaJurosCompostos = jurosCompostos(capitalInicial, taxaJuros, tempoAplicacao)
+
+console.log("Juros Simples:", calculaJurosSimples)
+console.log("Juros Compostos:", calculaJurosCompostos)
