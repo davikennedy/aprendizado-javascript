a0b4acaff549764d7c86f9e55e21e1064ae3a4cc
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2021-02-18 02:00:53 -0300

    Modificando arquivo numeros.js

--- Fundamentos/numeros.js
+++ Fundamentos/numeros.js
@@ -1,5 +1,5 @@
 const peso1 = 1.0
-const peso2 = Number('2.0')
+const peso2 = Number('2.0') // Transforma string em Number
 
 console.log(peso1, peso2)
 console.log(Number.isInteger(peso1))
@@ -12,8 +12,25 @@ const total = avaliacao1 * peso1 + avaliacao2 * peso2
 const media = total / (peso1 + peso2)
 
 console.log(media)
-console.log(media.toFixed(2))
+console.log(media.toFixed(2)) // Imprime com duas casas decimais
 console.log(media.toString(2)) // Imprime em binário
 
 console.log(typeof media)
 console.log(typeof Number)
+
+
+const meuNumero = 12.4032
+
+// Transformar número em string
+const numeroParaString = meuNumero.toString()
+console.log('Número transformado em string:', typeof numeroParaString)
+
+// Retorna número com casas decimais
+const numComCasasDecimais = meuNumero.toFixed(2)
+console.log('Número com duas casas decimais:', numComCasasDecimais)
+
+// Transforma uma string em float
+console.log('String convertida em float:', parseFloat('13.22'))
+
+// Transforma uma string em int
+console.log('String convertida em int:', parseInt('11.06'))
