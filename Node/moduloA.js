4f28973dd4d72b0cbff183836560513c4c1738c8
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2021-02-22 18:33:05 -0300

    :tada: Primeiros contatos com o sistema de módulos do Node.js

---
+++ Node/moduloA.js
@@ -0,0 +1,4 @@
+// Algumas maneiras de fazer exportação dos módulos
+this.ola = 'Fala pessoal!'
+exports.bemVindo = 'Bem-vindo ao Node!'
+module.exports.ateLogo = 'Até o próximo exemplo!' // Maneira mais clássica
---
+++ Node/moduloB.js
@@ -0,0 +1,7 @@
+// Maneira mais comum de fazer exportação dos módulos
+module.exports = {
+    bomDia: 'Bom dia!',
+    boaNoite() {
+        return 'Boa noite!'
+    }
+}
