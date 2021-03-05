bed50db1e9551541af17e845805b9578c103e890
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2021-03-04 21:29:08 -0300

    Modificando arquivo servidor.js

--- Node/Projeto/src/servidor.js
+++ Node/Projeto/src/servidor.js
@@ -1,9 +1,22 @@
 const porta = 3003
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bancoDeDados')
 
 app.get('/produtos', (req, res, next) => {
-    res.send({nome: 'Notebook', preco: 123.45}) // O método send retorna em formato JSON
+    res.send(bancoDeDados.getProdutos()) // O método send retorna em formato JSON
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+    res.send(produto)
 })
 
 app.listen(porta, () => {
