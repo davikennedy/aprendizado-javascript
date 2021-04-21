0d4c6e0edbc412714a3c16415c04b072b5100520
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2021-04-21 12:15:54 -0300

    Adicionando arquivo forOf.js

---
+++ Esnext/forOf.js
@@ -0,0 +1,37 @@
+// forOf: Percorre os valores
+for (let letra of "Cod3r") {
+    console.log(letra) // Imprime uma letra da palavra, por linha
+}
+
+const assuntosEcma = ['Map', 'Set', 'Promise']
+
+// forIn: Percorre os índices
+for (let i in assuntosEcma) {
+    console.log(i)
+}
+
+for (let assunto of assuntosEcma) {
+    console.log(assunto)
+}
+
+const assuntosMap = new Map([
+    ['Map', {abordado: true}],
+    ['Set', {abordado: true}],
+    ['Promise', {abordado: false}]  
+])
+
+for (let assunto of assuntosMap) {
+    console.log(assunto)
+}
+
+for (let chave of assuntosMap.keys()) {
+    console.log(chave)
+}
+
+for (let valor of assuntosMap.values()) {
+    console.log(valor)
+}
+
+for (let [chave, valor] of assuntosMap.entries()) {
+    console.log(chave, valor)
+}
