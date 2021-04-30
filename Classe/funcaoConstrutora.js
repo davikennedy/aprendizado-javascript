4fc1bf7bc20df30c92f006c94e1cce40f07d9c4c
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2021-04-30 11:27:14 -0300

    Adicionando arquivo funcaoConstrutora.js

---
+++ Classe/funcaoConstrutora.js
@@ -0,0 +1,10 @@
+function Car(brand, price) {
+    this.brand = brand
+    this.price = price
+}
+
+const civic = new Car('Honda', 130)
+const hb20 = new Car('Hyundai', 80)
+
+console.log(civic)
+console.log(hb20)
