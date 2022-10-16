0cd94ecf444d727358a27898bc40f2e1977d5d61
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-10-15 22:39:06 -0300

    Refatorando arquivo asyncAwait_2.js - Aprofundando o uso de Async/Await

--- Esnext/asyncAwait_2.js
+++ Esnext/asyncAwait_2.js
@@ -1,26 +1,48 @@
+/*
 function esperarPor(tempo = 2000) {
     return new Promise(function(resolve) {
         setTimeout(() => resolve(), tempo)
     })
 }
 
-/* esperarPor()
+esperarPor()
     .then(() => console.log('Executando promise 1...'))
     .then(esperarPor)
     .then(() => console.log('Executando promise 2...'))
     .then(esperarPor)
     .then(() => console.log('Executando promise 3...'))
-    .then(esperarPor) */
+    .then(esperarPor)
+*/
 
-async function executar() {
-    await esperarPor(1500)
-    console.log('Async/Await 1...')
-    
-    await esperarPor(1500)
-    console.log('Async/Await 2...')
-    
-    await esperarPor(1500)
-    console.log('Async/Await 3...')
+function retornarValor() {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(10, 5000))
+    })
 }
 
-executar()
+async function retornarValorRapido() {
+    return 20
+}
+
+async function executar() {
+//  let valor = await retornarValor()
+    let valor = await retornarValorRapido()
+    
+    await esperarPor(1500)
+    console.log(`Async/Await ${valor}...`)
+    
+    await esperarPor(1500)
+    console.log(`Async/Await ${valor + 1}...`)
+    
+    await esperarPor(1500)
+    console.log(`Async/Await ${valor + 2}...`)
+
+    return valor + 3
+}
+
+async function executarDeVerdade() {
+    const valor = await executar()
+    console.log(valor)
+}
+
+executarDeVerdade()
