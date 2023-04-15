fa7715bab5e269994c1e3b05b12176827da2dd27
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-04-15 16:13:12 -0300

    Relembrando conceitos de variáveis e operadores

---
+++ Variaveis e Operadores/aula.js
@@ -0,0 +1,12 @@
+let valor1 = 10;
+let valor2 = 5;
+
+let soma = valor1 + valor2;
+let subtracao = valor1 - valor2;
+let multiplicacao = valor1 * valor2;
+let divisao = valor1 / valor2;
+
+console.log(soma);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
