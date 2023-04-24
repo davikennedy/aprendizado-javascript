7a98248a2375b839f177c601a79ac51cf6245a1c
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-04-23 22:00:23 -0300

    Refatorando exercicio6.js

--- Exercícios Práticos/exercicio6.js
+++ Exercícios Práticos/exercicio6.js
@@ -5,22 +5,27 @@
 */
 
 function jurosSimples(capitalInicial, taxaJuros, tempoAplicacao) {
-    return capitalInicial + (capitalInicial / 100 * taxaJuros * tempoAplicacao)   
+    return capitalInicial + (capitalInicial / 100 * taxaJuros * tempoAplicacao);
 }
 
 function jurosCompostos(capitalInicial, taxaJuros, tempoAplicacao) {
-    let montante = 0
-    for(let i = 1; i <= tempoAplicacao; i++) {
-        montante = capitalInicial + (capitalInicial / 100 * taxaJuros)
-        capitalInicial = montante
+    let montante = 0;
+
+    for (let i = 1; i <= tempoAplicacao; i++) {
+        montante = capitalInicial + (capitalInicial / 100 * taxaJuros);
+        capitalInicial = montante;
     }
     return montante.toFixed(2)
 }
 
-const capitalInicial = 100, taxaJuros = 5, tempoAplicacao = 6
+function main() {
+    const capitalInicial = 100, taxaJuros = 5, tempoAplicacao = 6
 
-const calculaJurosSimples = jurosSimples(capitalInicial, taxaJuros, tempoAplicacao)
-const calculaJurosCompostos = jurosCompostos(capitalInicial, taxaJuros, tempoAplicacao)
+    const calculaJurosSimples = jurosSimples(capitalInicial, taxaJuros, tempoAplicacao);
+    const calculaJurosCompostos = jurosCompostos(capitalInicial, taxaJuros, tempoAplicacao);
 
-console.log("Juros Simples:", calculaJurosSimples)
-console.log("Juros Compostos:", calculaJurosCompostos)
+    console.log("Juros Simples:", calculaJurosSimples);
+    console.log("Juros Compostos:", calculaJurosCompostos);
+}
+
+main();
