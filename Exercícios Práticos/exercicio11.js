83a7a55949b03df8fe93edfade9dc91b1fb7cd1d
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-04-24 22:12:09 -0300

    Refatorando exercicio11.js

--- Exercícios Práticos/exercicio11.js
+++ Exercícios Práticos/exercicio11.js
@@ -2,12 +2,25 @@
     Desafio: Crie uma função que retorne o dia da semana com base na data atual
 */
 
+const DIAS_DA_SEMANA = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
+
 function diaDaSemana() {
     const dataAtual = new Date();
-    const diasDaSemana = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
-    const dia = diasDaSemana[dataAtual.getDay()];
+    const dia = DIAS_DA_SEMANA[dataAtual.getDay()];
+
+    if (!dia) {
+        throw new Error("Não foi possível determinar o dia da semana.");
+    }
 
     return dia;
 };
 
-console.log(`Hoje é ${diaDaSemana()}.`)
+function main() {
+    try {
+        console.log(`Hoje é ${diaDaSemana()}`);
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+main();
